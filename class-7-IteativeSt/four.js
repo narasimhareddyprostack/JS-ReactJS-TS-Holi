2ee0b8dd63b5860f29a4e7fd7e4a3ce086aefda7
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2026-03-09 11:17:38 +0530

    ts

---
+++ class-7-IteativeSt/four.js
@@ -0,0 +1,12 @@
+let enames=["Rahul","Sonai","Priyanka","Modi"]
+//using for while 
+
+/* let i=0;
+while(i<=enames.length-1){
+    console.log(enames[i])
+    i++;
+} */
+
+for(let ename of enames){
+    console.log(ename)
+}
---
+++ class-7-IteativeSt/three.js
@@ -0,0 +1,5 @@
+let enames=["Rahul","Sonai","Priyanka","Modi"]
+//using for loop
+for(let i=0;i<=enames.length-1;i++){
+    console.log(enames[i])
+}
