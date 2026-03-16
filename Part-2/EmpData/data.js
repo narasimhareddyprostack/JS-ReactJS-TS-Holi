d19586c3b946847370d7c3b13ec6c992b386333b
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2026-03-16 11:13:01 +0530

    ts

---
+++ Part-2/EmpData/data.js
@@ -0,0 +1,15 @@
+let employees=[
+    {eid:101,ename:"Rahul",esal:45000},
+    {eid:102,ename:"Sonia",esal:55000},
+    {eid:103,ename:"Priyanka",esal:65000},
+    {eid:104,ename:"Modi",esal:75000},
+    {eid:105,ename:"Amith",esal:85000},
+    {eid:106,ename:"DK shiva Kumar",esal:95000},
+]
+
+//create Javascript -
+//to display employee data in html table.
+function displayEmpData(){
+    //alert("Test Case 123")
+    document.getElementsByTagName('tbody')[0].innerHTML="GM"
+}
---
+++ Part-2/class-11-EventsDOM-Assignment/colors.js
@@ -0,0 +1,9 @@
+function changeColor5(){
+    document.getElementById('ip1').style.backgroundColor="yellow"
+    
+}
+function changeCase(){
+    let ename=document.getElementById('ip2').value;
+    document.getElementById('ip2').value=ename.toUpperCase();
+    
+}
