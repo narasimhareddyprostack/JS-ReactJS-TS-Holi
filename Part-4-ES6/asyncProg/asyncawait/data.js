0eb561778dba9494ce4b633cc9fadd17a10e9ba6
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2026-04-03 11:53:33 +0530

    ts

---
+++ Part-4-ES6/asyncProg/asyncawait/data.js
@@ -0,0 +1,31 @@
+let employees=[
+    {eid:101,ename:"Rahul",esal:45000.45},
+    {eid:102,ename:"Sonia",esal:55000.45}
+]
+let createEmployee=(emp)=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            let flag=true;
+            flag?resolve("Data Inserted"):reject("Failed")
+            employees.push(emp);
+        },4000)
+    })
+}
+let getEmployees=()=>{
+     setTimeout(()=>{
+        let rows="";
+        employees.forEach((emp)=>{
+            rows=rows+`<tr>
+                        <td>${emp.eid}</td>
+                        <td>${emp.ename}</td>
+                        <td>${emp.esal}</td>
+                        </tr>`
+        })
+        document.getElementById('tdata').innerHTML=rows;
+     },2000)
+}
+let execute = async()=>{
+    await createEmployee({eid:103,ename:"Priya",esal:65000})
+    getEmployees()
+}
+execute()
